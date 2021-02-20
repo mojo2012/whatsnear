0158b518f90f01060685be98d3202e05e3d6ba2d
Matthias Fuchs <matthias.fuchs@kps.com>
2021-02-20 08:08:24 +0100

    property decorator refactoring

--- frontend/src/components/ChatBubble/ChatBubble.ts
+++ frontend/src/components/ChatBubble/ChatBubble.ts
@@ -1,21 +1,26 @@
 /* eslint-disable max-classes-per-file */
-import { Options, prop, Vue } from "vue-class-component"
+import { Options, Vue } from "vue-class-component"
+import { Prop } from "vue-decorator"
 
-class Props {
-	public text = prop({
-		type: String,
-		required: true
-	})
-	public alignment = prop({
-		type: String,
-		required: true
-	})
-}
+// class Props {
+// 	public text = prop({
+// 		type: String,
+// 		required: true
+// 	})
+// 	public alignment = prop({
+// 		type: String,
+// 		required: true
+// 	})
+// }
 
 @Options({
 	name: "chat-bubble",
 	components: {}
 })
-export default class ChatBubble extends Vue.with(Props) {
-	//
+export default class ChatBubble extends Vue {
+	@Prop({ default: "", required: true, type: String })
+	public readonly text!: string
+
+	@Prop({ default: "left", required: true, type: String })
+	public readonly alignment!: string
 }
